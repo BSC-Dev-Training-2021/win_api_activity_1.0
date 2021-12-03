10404580907e2be1b58a9424d8b546293d462b92
CalciferNosk <eadriano.it@gmail.com>
2021-12-03 10:38:54 +0800

    gg

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>	</title>
+</head>
+<body>
+<form method="post" action="">	
+		<input type="text" name="fname">
+		<input type="text" name="lname">
+		<input type="text" name="email">
+
+		<button type="submit" name="submit">	</button>
+
+
+</form>
+
+</body>
+</html>
